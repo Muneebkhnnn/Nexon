b23ee278d1726acf44a6e1b40fd50b91aa8a76b0
Muneeb Khan <Muneebkhn0786@gmail.com>
2025-11-21 02:00:42 +0530

    finish

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,5 +1,4 @@
 import logo from "./logo.svg";
-import gradientBackground from '/gradientBackground.png'
 import user_group from "./user_group.png";
 import star_icon from "./star_icon.svg";
 import star_dull_icon from "./star_dull_icon.svg";
@@ -10,7 +9,7 @@ import { SquarePen, Hash, Image, Eraser, Scissors, FileText } from 'lucide-react
 
 export const assets = {
     logo,
-   gradientBackground,
+  
     user_group,
     star_icon,
     star_dull_icon,
